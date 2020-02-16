ed301ad3111ff234e1658e04480908a6a5e0f077
AnNiMuS42 <am@mlr.cc>
2020-02-16 16:10:09 +0100

    Initiales Commit

---
+++ Versionspruefung/module.php
@@ -0,0 +1,32 @@
+<?php
+// Klassendefinition
+class Versionspruefung extends IPSModule {
+
+    // Überschreibt die interne IPS_Create($id) Funktion
+    public function Create() {
+        // Diese Zeile nicht löschen.
+        parent::Create();
+
+        $this->RegisterVariableString("AktuelleVersion", "Aktuelle Version");
+
+    }
+
+    // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+    public function ApplyChanges() {
+        // Diese Zeile nicht löschen
+        parent::ApplyChanges();
+
+        $this->SetValue("AktuelleVersion", IPS_GetKernelVersion());
+    }
+
+    /**
+     * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+     * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+     *
+     * ABC_MeineErsteEigeneFunktion($id);
+     *
+     */
+    public function MeineErsteEigeneFunktion() {
+        // Selbsterstellter Code
+    }
+}
