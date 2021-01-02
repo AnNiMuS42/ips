10385bc8447cc34ce43fc669b5f43f3a2e4a14f7
Andre Mueller <am@mlr.cc>
2021-01-02 16:11:29 +0100

    New

--- Fensterkontakte/module.php
+++ Fensterkontakte/module.php
@@ -1,27 +1,24 @@
 <?php
 // Klassendefinition
-class Versionspruefung extends IPSModule {
+
+class Fensterkontakte extends IPSModule {
 
     // Überschreibt die interne IPS_Create($id) Funktion
     public function Create() {
-        // Diese Zeile nicht löschen.
+        // Diese Zeile nicht löschen. Wir aufgerufen, wenn Instanz erstellt wird oder IPS neu gestartet wird.
         parent::Create();
 
-        $this->RegisterVariableString("AktuelleVersion", $this->Translate("Current Version"));
-        $this->RegisterVariableString("VerfuegbareVersion", $this->Translate("Available Version"));
-        $this->RegisterPropertyInteger("UpdateIntervall", 12);
-        $this->RegisterAttributeInteger("LastUpdate",0);
-        $this->RegisterTimer("Update", 0, 'VP_UpdateVersion(' . $this->InstanceID . ');');
+        $this->RegisterVariableString("AktuelleVersion", "Aktuelle Version");
 
     }
 
     // Überschreibt die intere IPS_ApplyChanges($id) Funktion
     public function ApplyChanges() {
-        // Diese Zeile nicht löschen
+        // Diese Zeile nicht löschen. Wird immer dann aufgerufen, wenn Create aufgerufen wird und wenn Änderungen bestätigt werden.
         parent::ApplyChanges();
 
-        $this->SetTimerInterval("Update", $this->ReadPropertyInteger("UpdateIntervall") * 60 * 60 * 1000);
-        $this->UpdateVersion();
+        $this->SetValue("AktuelleVersion", IPS_GetKernelVersion());
+
     }
 
     /**
@@ -33,15 +30,5 @@ class Versionspruefung extends IPSModule {
      */
     public function UpdateVersion() {
 
-        $this->SetValue("AktuelleVersion", IPS_GetKernelVersion());
-
-        $rawData = file_get_contents('https://apt.symcon.de/dists/stable/win/binary-i386/Packages');
-        $xml = simplexml_load_string($rawData);
-        $version = $xml->channel->item->enclosure->attributes('sparkle',true)->shortVersionString;
-        $this->SetValue("VerfuegbareVersion", strval($version));
-
-        $updateTime = time();
-        $this->WriteAttributeInteger("LastUpdate", $updateTime);
-        $this->UpdateFormField("UpdateLabel", "caption", "Last Update: " . date("d.m.Y - h:m:s"));
     }
 }
